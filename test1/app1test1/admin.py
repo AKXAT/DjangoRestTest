671cd0ac7ed488a18e7c128e628f65b43ee28c32
AKXAT <akshat.aksharma@gmail.com>
2022-01-08 16:05:05 +0530

    registered for admin and add few data

--- test1/app1test1/admin.py
+++ test1/app1test1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Cities
 # Register your models here.
+
+@admin.register(Cities)
+class cityAdmin(admin.ModelAdmin):
+    list_to_display = ['Id','name','month','utilities'] 
