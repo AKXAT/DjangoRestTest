a16eee3e3c92dbf8f1ffa9b76c5c0d1b71aa0770
AKXAT <akshat.aksharma@gmail.com>
2022-01-08 15:47:38 +0530

    Added models and made the migrations

--- test1/app1test1/models.py
+++ test1/app1test1/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Cities(models.Model):
+    name = models.CharField(max_length=100)
+    month = models.CharField(max_length=3)
+    utilities = models.CharField(max_length=300)
